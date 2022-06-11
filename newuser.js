d455362025813f0c4bc0b01315bc4b8efd6310ed
chh4031 <chh4031@gmail.com>
2022-06-11 19:25:38 +0900

    기능구현과 어드민 페이지 css 추가

---
+++ newuser.js
@@ -0,0 +1,28 @@
+var username = document.getElementById("username");
+
+var doublearr = [];
+var k = 0;
+var i = 0;
+
+doublearr = doublearr.concat(JSON.parse(localStorage.getItem('z')));
+
+function user_sumbit(){
+    var username_value = username.value;
+
+    if(doublearr.includes(username_value)){
+        alert("값이 겹칩니다");
+    }else{
+        doublearr.push(username_value);
+
+        if(localStorage.getItem('k')!=0){
+            i = localStorage.getItem('k');
+            localStorage.setItem(i++, username_value);
+            localStorage.setItem('k', i);
+        }else{
+            localStorage.setItem(i++, username_value);
+            localStorage.setItem('k',i);
+        }
+        localStorage.setItem('z', JSON.stringify(doublearr));
+        alert('값 : ' + username_value + '이 저장되었습니다.');
+    }
+}
